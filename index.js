362cf78bb4fefcf3926f0e4c305fc9ba08de8b82
Anirudh Harit <haritanirudh@gmail.com>
2021-02-25 11:18:29 +0000

    cors try 3

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
-const cors = require('cors');
+// const cors = require('cors');
+// app.use(cors());
+
+// var express = require('express')
+var cors = require('cors');
+// var app = express()
+
 app.use(cors());
+
+// app.get('/products/:id', function (req, res, next) {
+//   res.json({msg: 'This is CORS-enabled for all origins!'})
+// })
+
+// app.listen(port, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 const port = process.env.PORT || config.get('port');
 const server = app.listen(port, () =>
